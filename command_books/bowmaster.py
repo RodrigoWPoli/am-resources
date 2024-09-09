0b171b2f74d9ce8d063ba8d3fe8e88c12da22dc3
RodrigoWPoli <rodtbag@gmail.com>
2024-09-09 17:47:02 -0300

    initial commit

---
+++ command_books/bowmaster.py
@@ -0,0 +1,307 @@
+"""A collection of all commands that Adele can use to interact with the game. 	"""
+
+from src.common import config, settings, utils
+import time
+import math
+from src.routine.components import Command
+from src.common.vkeys import press, key_down, key_up
+
+
+# List of key mappings
+class Key:
+    # Movement
+    JUMP = 'space'
+    FLASH_JUMP = 'space'
+    COVERING_FIRE = 'g'
+    BLINK_SHOT = 'f'
+
+    # Buffs
+    PHOENIX = 'v'
+
+    # Buffs Toggle
+    HOMIES = 'f1'
+    MIRAGE = 'f2'
+    GUIDED_ARROW = 'f3'
+
+    # Skills
+    ATTACK = 'ctrl'
+    ARROW_STREAM = 'a'
+    HURRICANE = 's'
+    GRITTY_GUST = 'd'
+    ORIGIN = 'q'
+    ARROW_PLATTER = 'w'
+    INHUMAN_SPEED = 'e'
+    QUIVER_BARRAGE = 'r'
+    STORM_OF_ARROWS = 't'
+    FURY_OF_THE_WILD = 'c'
+    ERDA_FOUNTAIN = 'z'
+    JANUS = 'x'
+    
+
+
+#########################
+#       Commands        #
+#########################
+def step(direction, target):
+    """
+    Performs one movement step in the given DIRECTION towards TARGET.
+    Should not press any arrow keys, as those are handled by Auto Maple.
+    """
+
+    num_presses = 2
+    if direction == 'up' or direction == 'down':
+        num_presses = 1
+    if config.stage_fright and direction != 'up' and utils.bernoulli(0.75):
+        time.sleep(utils.rand_float(0.1, 0.3))
+    d_y = target[1] - config.player_pos[1]
+    if abs(d_y) > settings.move_tolerance * 1.5:
+        if direction == 'down':
+            press(Key.JUMP, 3)
+        elif direction == 'up':
+            press(Key.JUMP, 1)
+    press(Key.FLASH_JUMP, num_presses)
+
+
+class Adjust(Command):
+    """Fine-tunes player position using small movements."""
+
+    def __init__(self, x, y, max_steps=5):
+        super().__init__(locals())
+        self.target = (float(x), float(y))
+        self.max_steps = settings.validate_nonnegative_int(max_steps)
+
+    def main(self):
+        counter = self.max_steps
+        toggle = True
+        error = utils.distance(config.player_pos, self.target)
+        while config.enabled and counter > 0 and error > settings.adjust_tolerance:
+            if toggle:
+                d_x = self.target[0] - config.player_pos[0]
+                threshold = settings.adjust_tolerance / math.sqrt(2)
+                if abs(d_x) > threshold:
+                    walk_counter = 0
+                    if d_x < 0:
+                        key_down('left')
+                        while config.enabled and d_x < -1 * threshold and walk_counter < 60:
+                            time.sleep(0.05)
+                            walk_counter += 1
+                            d_x = self.target[0] - config.player_pos[0]
+                        key_up('left')
+                    else:
+                        key_down('right')
+                        while config.enabled and d_x > threshold and walk_counter < 60:
+                            time.sleep(0.05)
+                            walk_counter += 1
+                            d_x = self.target[0] - config.player_pos[0]
+                        key_up('right')
+                    counter -= 1
+            else:
+                d_y = self.target[1] - config.player_pos[1]
+                if abs(d_y) > settings.adjust_tolerance / math.sqrt(2):
+                    if d_y < 0:
+                        FlashJump('up').main()
+                    else:
+                        key_down('down')
+                        time.sleep(0.05)
+                        press(Key.JUMP, 3, down_time=0.1)
+                        key_up('down')
+                        time.sleep(0.05)
+                    counter -= 1
+            error = utils.distance(config.player_pos, self.target)
+            toggle = not toggle
+
+
+class Buff(Command):
+
+    def __init__(self):
+        super().__init__(locals())
+        self.buff_time = 0
+
+    def main(self):
+        buffs = [Key.PHOENIX]
+        now = time.time()
+        if self.buff_time == 0 or now - self.buff_time > settings.buff_cooldown:
+            for key in buffs:
+                press(key, 3, up_time=0.3)
+            self.buff_time = now
+
+
+			
+class CoveringFire(Command):
+
+    def __init__(self, direction, jump='False'):
+        super().__init__(locals())
+        self.direction = settings.validate_arrows(direction)
+        self.jump = settings.validate_boolean(jump)
+
+    def main(self):
+        num_presses = 3
+        time.sleep(0.05)
+        key_down(self.direction)
+        time.sleep(0.05)
+        press(Key.COVERING_FIRE, num_presses)
+        key_up(self.direction)
+        if settings.record_layout:
+	        config.layout.add(*config.player_pos)
+
+class FlashJump(Command):
+    """Performs a flash jump in the given direction."""
+
+    def __init__(self, direction):
+        super().__init__(locals())
+        self.direction = settings.validate_arrows(direction)
+
+    def main(self):
+        key_down(self.direction)
+        time.sleep(0.1)
+        press(Key.FLASH_JUMP, 1)
+        if self.direction == 'up':
+            press(Key.FLASH_JUMP, 1)
+        else:
+            press(Key.FLASH_JUMP, 1)
+        key_up(self.direction)
+        time.sleep(0.5)
+			
+
+class Stream(Command):
+    """Attacks using 'Arrow Stream' in a given direction."""
+
+    def __init__(self, direction, attacks=2, repetitions=1):
+        super().__init__(locals())
+        self.direction = settings.validate_horizontal_arrows(direction)
+        self.attacks = int(attacks)
+        self.repetitions = int(repetitions)
+
+    def main(self):
+        time.sleep(0.05)
+        key_down(self.direction)
+        time.sleep(0.05)
+        if config.stage_fright and utils.bernoulli(0.7):
+            time.sleep(utils.rand_float(0.1, 0.3))
+        for _ in range(self.repetitions):
+            press(Key.ARROW_STREAM, self.attacks, up_time=0.05)
+        key_up(self.direction)
+        if self.attacks > 2:
+            time.sleep(0.3)
+        else:
+            time.sleep(0.2)
+
+class portal(Command):
+    """Uses portal."""
+
+    def main(self):
+        press('up', 1, up_time=0.05)
+
+class GrittyGust(Command):
+    """Uses 'Gritty Gust' once."""
+
+    def main(self):
+        press(Key.GRITTY_GUST, 1, up_time=0.05)
+
+class Hurricane(Command):
+    """Holds down 'Hurricane' until this command is called again."""
+
+    def main(self):
+        key_up(Key.HURRICANE)
+        time.sleep(0.075)
+        key_down(Key.HURRICANE)
+        time.sleep(0.15)
+			
+class ErdaFountain(Command):
+    """
+    Places 'Erda fountain' in a given direction, or towards the center of the map if
+    no direction is specified.
+    """
+
+    def __init__(self, direction=None):
+        super().__init__(locals())
+        if direction is None:
+            self.direction = direction
+        else:
+            self.direction = settings.validate_horizontal_arrows(direction)
+
+    def main(self):
+        if self.direction:
+            press(self.direction, 1, down_time=0.1, up_time=0.05)
+        else:
+            if config.player_pos[0] > 0.5:
+                press('left', 1, down_time=0.1, up_time=0.05)
+            else:
+                press('right', 1, down_time=0.1, up_time=0.05)
+        press(Key.ERDA_FOUNTAIN, 3)
+        if settings.record_layout:
+	        config.layout.add(*config.player_pos)
+
+
+class ArrowPlatterFixed(Command):
+    """
+    Uses 'ArrowPlatterFixed' in a given direction
+    """
+
+    def __init__(self, direction=None):
+        super().__init__(locals())
+        if direction is None:
+            self.direction = direction
+        else:
+            self.direction = settings.validate_horizontal_arrows(direction)
+
+    def main(self):
+        if self.direction:
+            press(self.direction, 1, down_time=0.1, up_time=0.05)
+        self.toggle()
+        time.sleep(0.3)
+        press(Key.ATTACK, 1, up_time=0.05)
+        time.sleep(0.3)
+        self.toggle()
+
+    def toggle(self):
+        key_down(Key.ARROW_PLATTER)
+        time.sleep(0.075)
+        key_up(Key.ARROW_PLATTER)
+        time.sleep(0.15)
+
+class ArrowPlatter(Command):    
+    """Holds down 'Arrow Platter' until this command is called again."""
+    
+    def main(self):
+        key_up(Key.ARROW_PLATTER)
+        time.sleep(0.075)
+        key_down(Key.ARROW_PLATTER)
+        time.sleep(0.15)
+
+
+class InhumanSpeed(Command):
+    """Uses 'Inhuman Speed' once."""
+
+    def main(self):
+        press(Key.INHUMAN_SPEED, 2, up_time=0.05)
+
+class QuiverBarrage(Command):
+    """Uses 'Quiver Barrage' once."""
+
+    def main(self):
+        press(Key.QUIVER_BARRAGE, 2, up_time=0.05)
+
+class StormOfArrows(Command):
+    """Uses 'Storm of Arrows' once."""
+
+    def main(self):
+        press(Key.STORM_OF_ARROWS, 2, up_time=0.05)
+
+class FuryOfTheWild(Command):
+    """Uses 'Fury of the Wild' once."""
+
+    def main(self):
+        press(Key.FURY_OF_THE_WILD, 2, up_time=0.05)
+
+class Janus(Command):
+    """Uses 'Janus' once."""
+
+    def main(self):
+        press(Key.JANUS, 2, up_time=0.05)
+
+class Origin(Command):
+    """Uses 'Origin' once."""
+
+    def main(self):
+        press(Key.ORIGIN, 2, up_time=0.05)
